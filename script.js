f3021cadc094da259b67a14dd1e8c2f05d570216
Stephen <stephen.fu.hang.choi@gmail.com>
2018-01-02 00:36:30 -0500

    added responsiveness, missing index.html

--- script.js
+++ script.js
@@ -1,42 +1,126 @@
-// index.html
+// business.html
+var circle = document.getElementsByClassName('clicke1');
 
-// changing description using typeit.js
+var ite = 0;
+var circ = null;
 
-$('.dash').typeIt({
-	 speed: 125,
-	 deleteSpeed: 125,
-	 breakLines: false,
-	 autoStart: false,
-})
-.tiType('Developer')
-.tiPause(2000)
-.tiDelete(9)
-.tiPause(400)
-.tiType('Entrepreneur')
-.tiPause(2000)
-.tiDelete(12)
-.tiPause(400)
-.tiType('Student')
-.tiPause(2000)
-.tiDelete(7)
-.tiPause(400)
-.tiType('Hacker')
-.tiPause(2000)
-.tiDelete(6)
-.tiPause(400)
-.tiType('Developer')
+function changecolor() {
+	if ($(window).width() < 1116) {
+		circ = circle[1];
+	} else {
+		circ = circle[0];
+	}
 
+	if (ite == 0) {
+		circ.style.opacity = 0.3;
+		circ.style.height = "85px";
+		circ.style.width = "85px";
+		circ.style.marginLeft = "-5px";
+		ite++;
+	} else {
+		circ.style.opacity = 1;
+		circ.style.height = "70px";
+		circ.style.width = "70px";
+		circ.style.marginLeft = "0px";
+		ite = 0;
+	}
+};
 
-// code.html
-// $(document).ready(function(){
-// 	$('#cta-pic').hover(function(){
-// 		$('#cta-pic').css("opacity", "0.4");
-// 	},
-// 	function(){
-// 		$('#cta-pic').css("opacity", "1");
-// 	});
-// });
+var help = window.setInterval(changecolor,1200);
 
+//general hover
+var caption_hover = {
+	"transform": "translateY(0)",
+	"opacity": "1"
+}
+
+// leangap hover
+var style_hover_1 = {
+	"background-color": "#472C45",
+	"box-shadow": "0 0 0 20px #1b2835"
+};
+
+$(document).ready(function(){
+	$(".style1").on('mouseenter click', function() {
+		$(".style1 img").css("transform", "translateY(-110px)"),
+		$(".style1 .business-cap").css(caption_hover),
+		$(".style1").css(style_hover_1),
+		clearInterval(help),
+		$(".clicke1").css('opacity', '0');
+	});
+});
+
+// deca hover 
+var style_hover_2 = {
+	"background-color": "#dbb3ff",
+	"box-shadow": "0 0 0 20px #0c539b"
+};
+
+$(document).ready(function(){
+	$(".style2").on('mouseenter click', function() {
+		$(".style2 img").css("transform", "translateY(-100px)"),
+		$(".style2 .business-cap").css(caption_hover),
+		$(".style2").css(style_hover_2);
+	});
+});
+
+// FBLA hover 
+var style_hover_3 = {
+	"background-color": "#8c0333",
+	"box-shadow": "0 0 0 20px #e61060"
+};
+
+$(document).ready(function(){
+	$(".style3").on('mouseenter click', function() {
+		$(".style3 img").css("transform", "translateY(-100px)"),
+		$(".style3 .business-cap").css(caption_hover),
+		$(".style3").css(style_hover_3);
+	});
+});
+
+// CTA hover
+var style_hover_4 = {
+	"background-color": "#e2cdad",
+	"box-shadow": "0 0 0 20px #f6921e"
+};
+
+$(document).ready(function(){
+	$(".style4").on('mouseenter click', function() {
+		$(".style4 img").css("transform", "translateY(-120px)"),
+		$(".style4 .bottom-cap").css(caption_hover),
+		$(".style4").css(style_hover_4);
+	});
+});
+
+// E3 hover 
+var style_hover_5 = {
+	"background-color": "#9e7c89",
+	"box-shadow": "0 0 0 20px #754f5c"
+};
+
+$(document).ready(function(){
+	$(".style5").on('mouseenter click', function() {
+		$(".style5 img").css("transform", "translateY(-120px)"),
+		$(".style5 .bottom-cap").css(caption_hover),
+		$(".style5").css(style_hover_5);
+	});
+});
+
+// P5k hover
+var style_hover_6 = {
+	"background-color": "#2b2142",
+	"box-shadow": "0 0 0 20px #3884c5"
+};
+
+$(document).ready(function(){
+	$(".style6").on('mouseenter click', function() {
+		$(".style6 img").css("transform", "translateY(-120px)"),
+		$(".style6 .bottom-cap").css(caption_hover),
+		$(".style6").css(style_hover_6);
+	});
+});
+
+// code
 $(document).ready(function(){
 	// array with image urls 
 	var dataArray=new Array();
@@ -54,155 +138,14 @@ $(document).ready(function(){
 	});
 });
 
-
-// business.html
-
-//general hover
-var caption_hover = {
-	"transform": "translateY(0)",
-	"opacity": "1"
-};
-
-// leangap hover
-var style_hover_1 = {
-	"background-color": "#472C45",
-	"box-shadow": "0 0 0 20px #1b2835"
-}
 $(document).ready(function(){
-	$(".style1").hover(function() {
-		$(".style1 img").css("transform", "translateY(-110px)"),
-		$(".style1 .business-cap").css(caption_hover);
-		$(".style1").css(style_hover_1)
-	});
-});
-
-// deca hover 
-var style_hover_2 = {
-	"background-color": "#dbb3ff",
-	"box-shadow": "0 0 0 20px #0c539b"
-}
-
-$(document).ready(function(){
-	$(".style2").hover(function() {
-		$(".style2 img").css("transform", "translateY(-100px)"),
-		$(".style2 .business-cap").css(caption_hover);
-		$(".style2").css(style_hover_2)
-	});
-});
-
-// FBLA hover 
-var style_hover_3 = {
-	"background-color": "#8e1642",
-	"box-shadow": "0 0 0 20px #e61060"
-}
-
-$(document).ready(function(){
-	$(".style3").hover(function() {
-		$(".style3 img").css("transform", "translateY(-100px)"),
-		$(".style3 .business-cap").css(caption_hover);
-		$(".style3").css(style_hover_3)
-	});
-});
-
-// CTA hover
-var style_hover_4 = {
-	"background-color": "#e2cdad",
-	"box-shadow": "0 0 0 20px #f6921e"
-}
-
-$(document).ready(function(){
-	$(".style4").hover(function() {
-		$(".style4 img").css("transform", "translateY(-120px)"),
-		$(".style4 .bottom-cap").css(caption_hover),
-		$(".style4").css(style_hover_4)
-	});
-});
-
-// E3 hover 
-var style_hover_5 = {
-	"background-color": "#9e7c89",
-	"box-shadow": "0 0 0 20px #754f5c"
-}
-
-$(document).ready(function(){
-	$(".style5").hover(function() {
-		$(".style5 img").css("transform", "translateY(-120px)"),
-		$(".style5 .bottom-cap").css(caption_hover),
-		$(".style5").css(style_hover_5)
-	});
-});
-
-// P5k hover
-var style_hover_6 = {
-	"background-color": "#2b2142",
-	"box-shadow": "0 0 0 20px #3884c5"
-}
-
-$(document).ready(function(){
-	$(".style6").hover(function() {
-		$(".style6 img").css("transform", "translateY(-120px)"),
-		$(".style6 .bottom-cap").css(caption_hover),
-		$(".style6").css(style_hover_6)
-	});
-});
-
-
-
-
-
-
-
-// contact section
-// makes it slower but requires color plugin $(".img-div-mail").animate({'background-color': 'black'}, 'slow'),
-$(document).ready(function(){
-	$(".img-div-github").hover(function(){
-		$(".img-div-mail").css("background-color", "black"),
-		$(".img-div-linkedin").css("background-color", "black"),
-		$(".img-div-resume").css("background-color", "black");
+	$(".view-cont").hover(function() {
+		$(".pics").css("opacity", "0.4"),
+		$(".view").css("opacity", "1");
 	},
 	function(){
-		$(".img-div-mail").css("background-color", "#C4C4C4"),
-		$(".img-div-linkedin").css("background-color", "#C4C4C4"),
-		$(".img-div-resume").css("background-color", "#C4C4C4");
-	});
-});
-
-$(document).ready(function(){
-	$(".img-div-mail").hover(function(){
-		$(".img-div-github").css("background-color", "black"),
-		$(".img-div-linkedin").css("background-color", "black"),
-		$(".img-div-resume").css("background-color", "black");
-	},
-	function(){
-		$(".img-div-github").css("background-color", "#C4C4C4"),
-		$(".img-div-linkedin").css("background-color", "#C4C4C4"),
-		$(".img-div-resume").css("background-color", "#C4C4C4");
-	});
-});
-
-$(document).ready(function(){
-	$(".img-div-linkedin").hover(function(){
-		$(".img-div-github").css("background-color", "black"),
-		$(".img-div-mail").css("background-color", "black"),
-		$(".img-div-resume").css("background-color", "black");
-	},
-	function(){
-		$(".img-div-mail").css("background-color", "#C4C4C4"),
-		$(".img-div-github").css("background-color", "#C4C4C4"),
-		$(".img-div-resume").css("background-color", "#C4C4C4");
-	});
-});
-
-$(document).ready(function(){
-	$(".img-div-resume").hover(function(){
-		$(".img-div-github").css("background-color", "black"),
-		$(".img-div-mail").css("background-color", "black"),
-		$(".img-div-linkedin").css("background-color", "black");
-	},
-	function(){
-		$(".img-div-mail").css("background-color", "#C4C4C4"),
-		$(".img-div-github").css("background-color", "#C4C4C4"),
-		$(".img-div-linkedin").css("background-color", "#C4C4C4");
+		$(".pics").css("opacity", "1"),
+		$(".view").css("opacity", "0");
 	});
 });
 
